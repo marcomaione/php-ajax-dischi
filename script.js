89f1591f39af0632ce6125ce2d1d7043d8848c98
marco maione <maionemarco14@gmail.com>
2022-03-14 18:00:43 +0100

    aggiornamento

---
+++ script.js
@@ -0,0 +1,19 @@
+const app = new Vue(
+    {
+        el: "app",
+        data : {
+            albums : []
+        },
+        methods: {
+            getAlbums(){
+                axios.get('http://localhost/php-ajax-dischi/database.php')
+                .then((response) => {
+                    this.albums.push(response.data.response);
+                });
+
+            }
+
+            
+        },
+    }
+)
